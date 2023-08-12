6d6a530a2b86a42d0792dfdfc86c5df496bbeab4
junwon <jjuny9798@gmail.com>
2023-08-12 19:01:10 +0900

    feat: add logging file

--- README.md
+++ README.md
@@ -3,11 +3,20 @@
 - boostcamp
 
     - [노션](https://www.notion.so/junwon-0313/FastAPI-7acc8b3061e14f1781b5cfa54a6d1c41): Private
-    
+
+    - [GCP](https://console.cloud.google.com/) : Logging with BigQuery
+
+        1. 빅쿼리에 테이블을 세팅
+        2. 빅쿼리에 적재하기 쉽게 JSON 형태로 로그를 정제
+        3. python logging 모듈을 사용해, 빅쿼리에 실시간 로그 적재
+        4. console과 file에도 기록이 남도록 handler를 지정
+
+        서비스 계정을 적절한 권한을 부여해 생성하고 키를 다운 받아 사용해야한다!
+
+---
 
 - tutorial
 
     - [FastAPI](https://fastapi.tiangolo.com/ko/tutorial/)
 
     - [Pydantic](https://docs.pydantic.dev/latest/)
-
--- boostcamp/python_logging.py
+++ boostcamp/python_logging.py
@@ -1,6 +1,7 @@
 #### 1. logging module 써보기
 import logging
 
+#loggin은 기본적으로 warning부터 출력되게 설정되어 있음.
 logger = logging.getLogger("example")  # root logger
 logger.info("hello world")  # 아무런 로그도 출력되지 않습니다.
 
@@ -26,3 +27,12 @@ logger_config = {
 logging.config.dictConfig(logger_config)
 logger_with_config = logging.getLogger("example")
 logger_with_config.info("이제는 보이죠?")
+
+#### 1.2 dynamic logging config
+dynamic_logger = logging.getLogger()
+log_handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s | %(levelname)s - %(message)s")
+log_handler.setFormatter(formatter)
+dynamic_logger.addHandler(log_handler)
+
+dynamic_logger.info("hello world")
